fc39c47bb355a44023a4158f4d3ae581392a9d56
Manar16671 <151847677+Manar16671@users.noreply.github.com>
2024-02-14 16:32:09 +0300

    routes name update

--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-route::post('Register', 'App\Http\Controllers\AuthController@register');
-route::post('Login', 'App\Http\Controllers\AuthController@login');
+route::post('register', 'App\Http\Controllers\AuthController@register');
+route::post('login', 'App\Http\Controllers\AuthController@login');
 use App\Http\Controllers\UserController;
 
 Route::get('/users/{id}', [UserController::class, 'getUserById']);
